375a2be14b045bd81adc6a6d56b40f41b8e49b6b
logan <logan980102@gmail.com>
2025-02-28 22:03:56 +0900

    타임테스트, 추가 버튼 색상 변경

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def reset_todos():
 
 # ✅ 스케줄러 실행 (매일 6시)
 scheduler = BackgroundScheduler()
-scheduler.add_job(reset_todos, "cron", hour=6, timezone="Asia/Seoul")
+scheduler.add_job(reset_todos, "cron", hour=22, minute=5, timezone="Asia/Seoul") 
 scheduler.start()
 
 # ✅ 투두리스트 조회
